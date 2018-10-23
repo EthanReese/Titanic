38dd92e96853c1474999b980bb9ca17ee05715ae
EthanReese <ethan.reese01@gmail.com>
2018-10-22 20:40:55 -0700

    Added basic functionality

--- Main.py
+++ Main.py
@@ -3,9 +3,32 @@
 #October 18, 2018
 
 import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
 
-data = pd.read_csv('C:/Users/298970/train.csv')
+data = pd.read_csv('/Users/Ethan/Devel/data/Titanic/train.csv')
 
-features = ["pclass", "sex", "Age", "parch"]
+np.append(data, np.zeros((data['Sex'].length, 1), dtype=int64)
 
-X = data[features];
+for i in data['Sex']:
+    if data['Sex'][i] == "female":
+        data['Sex'][i] = 0
+    else:
+        X['Sex'][i] = 1
+
+
+features = ['Pclass', 'Sex', 'Age', 'Parch']
+
+X = data[features]
+
+y = data.Survived
+
+data = data.dropna(axis=0)
+
+model = DecisionTreeRegressor(random_state = 1)
+
+#model.fit(X,y)
+
+X.head()
+
+#model.predict(X.head())
